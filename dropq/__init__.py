d848bdc7f17877cc81b1a120bb8470ed42955321
talumbau <tj.alumbaugh@continuum.io>
2016-10-13 14:30:37 -0500

    Accept file-based reforms for elasticity of GDP
     - File-based reforms could generate reform dictionaries of the style:
    
      {year: {reform}, year+1: {reform}... }
    
      instead of the style generated by the web-based TaxBrain input page
    
      {year: {reform....}}
    
      where reforms in later years are sequential values of the particular
      reform value.
    
      add test to still grab the proper elasticity of GDP for year_n from
      the reform

--- dropq/__init__.py
+++ dropq/__init__.py
@@ -2,7 +2,7 @@ from .dropq import (run_models, groupby_means_and_comparisons, run_nth_year,
                     only_growth_assumptions, only_behavior_assumptions,
                     only_reform_mods, example_data, format_macro_results,
                     run_nth_year_mtr_calc, run_gdp_elast_models,
-                    get_unknown_parameters)
+                    get_unknown_parameters, elasticity_of_gdp_year_n)
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
